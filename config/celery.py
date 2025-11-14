8670891d009a99d26be0add4fc2575abbf69b310
Flynnt Knapp <bruce.stull@gmail.com>
2025-11-14 04:45:37 -0500

    Added 'config/celery.py' and module contents for usage of celery in the application.

---
+++ config/celery.py
@@ -0,0 +1,16 @@
+# config/celery.py
+
+import os
+from celery import Celery
+from django.conf import settings
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.dev")
+# In production, set env var: DJANGO_SETTINGS_MODULE=config.settings.prod
+
+app = Celery("config")
+app.config_from_object("django.conf:settings", namespace="CELERY")
+app.conf.enable_utc = True
+app.conf.timezone = settings.TIME_ZONE
+
+app.autodiscover_tasks()
+app.conf.beat_scheduler = "django_celery_beat.schedulers:DatabaseScheduler"
