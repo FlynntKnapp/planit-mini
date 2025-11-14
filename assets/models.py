a1aa1467aefac23cbcd14d60aae69ad99f6eb2fd
Flynnt Knapp <bruce.stull@gmail.com>
2025-11-14 17:41:10 -0500

    Add initial models to 'assets' app.

--- assets/models.py
+++ assets/models.py
@@ -1,3 +1,39 @@
-from django.db import models  # noqa: F401
+# assets/models.py
 
-# Create your models here.
+from django.db import models
+
+
+class FormFactor(models.Model):
+    name = models.CharField(max_length=80, unique=True)
+    slug = models.SlugField(unique=True)
+
+
+class OS(models.Model):
+    name = models.CharField(max_length=80)
+    version = models.CharField(max_length=40, blank=True)
+    slug = models.SlugField(unique=True)
+
+
+class Application(models.Model):
+    name = models.CharField(max_length=120)
+    version = models.CharField(max_length=60, blank=True)
+    slug = models.SlugField(unique=True)
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=120, unique=True)
+    description = models.TextField(blank=True)
+    slug = models.SlugField(unique=True)
+
+
+class Asset(models.Model):
+    KIND_CHOICES = [("PI", "Raspberry Pi"), ("SRV", "Server"), ("LAP", "Laptop")]
+    name = models.CharField(max_length=120)
+    kind = models.CharField(max_length=3, choices=KIND_CHOICES)
+    form_factor = models.ForeignKey(FormFactor, null=True, on_delete=models.SET_NULL)
+    os = models.ForeignKey(OS, null=True, on_delete=models.SET_NULL)
+    applications = models.ManyToManyField(Application, blank=True)
+    location = models.CharField(max_length=120, blank=True)
+    purchase_date = models.DateField(null=True, blank=True)
+    warranty_expires = models.DateField(null=True, blank=True)
+    notes = models.TextField(blank=True)
